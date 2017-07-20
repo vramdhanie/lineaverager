9105897bf8f8b1477ca4d2c45f0bf1b0723796ba
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-07-20 09:54:12 -0400

    Initialize repository with create-react-app

---
+++ README.md
@@ -0,0 +1,14 @@
+# Line Averager
+Are you ever waiting in line at the bank and wonder how long it will take you to get to the
+front of the line? Well wonder no more...the line averager will take the number of people
+in the line, and continuously update the estimated length of time it takes
+by using the mean time between exits.
+
+## Documentation 
+
+
+## Acknowledgement
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## License
+MIT
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Line Averager</h2>
+        </div>
+        <p className="App-intro">
+          Estimate the time to get to the head of a line!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
