9d9c7eeff9293f5d641f395612844f2dd0169ecf
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-01 14:32:36 -0400

    Add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+import * as actions from '../actions';
+
+const STATE = {
+    RUNNING:'RUNNING',
+    STOPPED:'STOPPED',
+    PAUSED:'PAUSED'
+}
+
+export const initialState = {
+    timer_state: STATE.STOPPED
+};
+
+export const timerReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actions.START_TIMER:
+            return {...state, timer_state: STATE.RUNNING};
+        default:
+            return state;
+    }
+}
