a65f629738df132066d75601f9919fd62870f6d3
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-17 13:28:51 -0400

    Fix layout of IntemCount

--- src/components/itemcount/ItemCount.js
+++ src/components/itemcount/ItemCount.js
@@ -4,7 +4,10 @@ import {connect} from "react-redux";
 import { incrementCount, decrementCount } from "../../actions/index";
 import upArrow from './up.svg';
 import downArrow from './down.svg';
-import { Container, Row, Col, ButtonGroup, Button, Card } from 'reactstrap';
+import {
+    Container, Row, Col, ButtonGroup, Button, Card, InputGroup, InputGroupButton,
+    InputGroupAddon
+} from 'reactstrap';
 
 
 class ItemCount extends Component {
@@ -29,21 +32,19 @@ class ItemCount extends Component {
             <div className="ItemCount">
                 <Container>
                     <Row noGutters>
-                        <Col xs={{size:3, offset: 4}}>
-
-                                <div className="ItemCount__count">{this.props.count}</div>
-
-                        </Col>
-                        <Col xs={{size:3}}>
-                            <ButtonGroup vertical>
-                                <Button onClick={this.up}>UP</Button>
-                                <Button onClick={this.down}>DN</Button>
-                            </ButtonGroup>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <div className="ItemCount__subtle">{ this.props.label }</div>
+                        <Col md={{size:4, offset: 4}}>
+                            <InputGroup>
+                                <InputGroupButton>
+                                    <Button onClick={this.up}>UP</Button>
+                                </InputGroupButton>
+                                <InputGroupAddon>
+                                    <div className="ItemCount__count">{this.props.count}</div>
+                                    <div className="ItemCount__subtle">{ this.props.label }</div>
+                                </InputGroupAddon>
+                                <InputGroupButton>
+                                    <Button onClick={this.down}>DN</Button>
+                                </InputGroupButton>
+                            </InputGroup>
                         </Col>
                     </Row>
                 </Container>
